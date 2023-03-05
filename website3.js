03594ea7b1425536a2531fb59b1b42bdb23af314
TrupleCpu <126572562+TrupleCpu@users.noreply.github.com>
2023-03-05 22:34:19 +0800

    Add files via upload

--- website3.js
+++ website3.js
@@ -1,3 +1,19 @@
+const openPop = document.getElementById("Users");
+const PopOpen = document.getElementById("popGo");
+const move = false;
+let popExit = document.getElementById("exit");
+
+openPop.addEventListener("click", function(){
+   if(!move){
+    PopOpen.style.marginTop = "120px";
+   } else {
+    PopOpen.style.marginTop = "-10000px";
+   }
+   move = !move;
+})
+popExit.addEventListener("click", function(){
+    PopOpen.style.marginTop = "-10000px";
+})
 const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 3000,
@@ -16,3 +32,6 @@ prevEl: '.swiper-button-prev',
 },
 
 });
+
+
+
